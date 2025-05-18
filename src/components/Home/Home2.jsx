47730aed3402a14a1277a993a53234b6ab5435c8
Priyanshu Sahoo <94666105+Priyanshu1184@users.noreply.github.com>
2025-05-18 16:57:38 +0530

    update the font style

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -23,29 +23,29 @@ function Home2() {
               something, I think… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
-              <i>
+              
                 <b className="purple"> C, Python </b>
-              </i>
+              
               <br />
               <br />
               My field of Interest's are building new &nbsp;
-              <i>
+              
                 <b className="purple">Web Technologies and Products </b>
-              </i>
+              
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
               with <b className="purple">Node.js</b> and
-              <i>
+              
                 <b className="purple">
                   {" "}
                   Modern Javascript Library and Frameworks
                 </b>
-              </i>
+              
               &nbsp; like
-              <i>
+              
                 <b className="purple"> React.js and Next.js</b>
-              </i>
+              
             </p>
           </Col>
           <Col md={4} className="myAvtar">
