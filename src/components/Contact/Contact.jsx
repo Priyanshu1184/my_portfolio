43c2fffb94ec710b2adb98c61b7caef78047829a
Priyanshu Sahoo <94666105+Priyanshu1184@users.noreply.github.com>
2025-05-18 16:29:04 +0530

    update the env file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -26,10 +26,10 @@ function Contact() {
     const currentTime = new Date().toLocaleString();
 
     emailjs.send(
-      process.env.service_ID, // Replace with your EmailJS service ID
-      process.env.template_ID, // Replace with your EmailJS template ID
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
       formData,
-      process.env.user_ID // Replace with your EmailJS user ID
+      process.env.REACT_APP_USER_ID
     )
     .then((response) => {
       setStatus("SUCCESS");
